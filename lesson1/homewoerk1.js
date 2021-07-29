85b9cdfd1449a33857d790925f245ccc97db7f7e
hakkhachatryan03 <hakob.kh.2003@gmail.com>
2021-07-29 23:52:00 +0400

    lesson1

---
+++ lesson1/homewoerk1.js
@@ -0,0 +1,64 @@
+// //N.1
+// let a = 5;
+// let b = 10;
+// if (a % b === 0 || b % a === 0) {
+//     console.log(1);
+// } else {
+//     console.log(0);
+// }
+//
+// //NO 3
+// let a = +prompt("a = " );
+// let b = Math.pow(a, 2);
+// let c = Math.pow(a, 3);
+// let z  = a + b + c;
+// console.log(z);
+//
+// //N.5
+// let arr = [40, 50, 60, 70, 24]
+// arr.map(i=>x+=i, x = 0);
+// (5) [40, 90, 150, 220, 244]
+// arr.map(i=>x+=i, x = 0).reverse();
+// arr.map(i=>x+=i, x = 0).reverse()[0];
+// 244
+// arr.map(i=>x+=i, x = 0).reverse()[0]/arr.length;
+//
+// /* NO.6*/
+// let a = +prompt("a = ");
+// if (a % 3 === 0 && a % 5 === 0 && a % 7 === 0) {
+//     console.log("bajanvum e 3, 5 ,7");
+// } else if (a % 3 === 0 && a % 5 === 0) {
+//     console.log("3, 5");
+// } else if (a % 3 === 0 && a % 7 === 0) {
+//     console.log("3, 7");
+// } else if (a % 5 === 0 && a % 7 === 0) {
+//     console.log("5, 7");
+// } else if (a % 5 === 0) {
+//     console.log("5-i");
+// } else if (a % 3 === 0) {
+//     console.log("3-i ");
+// } else if (a % 7 === 0) {
+//     console.log("7-i");
+// } else {
+//     console.log("chkpav");
+// }
+//
+// //N.7
+// mounth = +prompt("mounth = ");
+// ear = +prompt("ear = ");
+// function age() {
+//     if (mounth < 8 && ear == 0) {
+//         console.log("bayb");
+//     } else if (mounth > 0 && ear > 0 && ear < 7) {
+//         console.log("sred");
+//     } else if (mounth > 0 && ear > 7 && ear < 18 ) {
+//         console.log("hamarya chapahas");
+//     }
+// }
+// /* */
+// a = +prompt('a = ');
+// if (typeof(a) == number) {
+//     console.log('a');
+// } else {
+//     console.log("a is not number");
+// }
